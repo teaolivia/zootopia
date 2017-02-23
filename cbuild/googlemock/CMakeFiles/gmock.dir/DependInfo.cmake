46f05ddf8ccf2cdd204dd2538badb6550aea6939
atianadya <theaolivia.mail@gmail.com>
2017-02-24 00:10:44 +0700

    added gtest & cppcheck

---
+++ cbuild/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/theaolivia/GitHub/zootopia/googletest/googletest/src/gtest-all.cc" "/Users/theaolivia/GitHub/zootopia/cbuild/googlemock/CMakeFiles/gmock.dir/__/googletest/src/gtest-all.cc.o"
+  "/Users/theaolivia/GitHub/zootopia/googletest/googlemock/src/gmock-all.cc" "/Users/theaolivia/GitHub/zootopia/cbuild/googlemock/CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/theaolivia/GitHub/zootopia/googletest/googlemock/include"
+  "/Users/theaolivia/GitHub/zootopia/googletest/googlemock"
+  "/Users/theaolivia/GitHub/zootopia/googletest/googletest/include"
+  "/Users/theaolivia/GitHub/zootopia/googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cbuild/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/theaolivia/GitHub/zootopia/googletest/googletest
+# Build directory: /Users/theaolivia/GitHub/zootopia/cbuild/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
