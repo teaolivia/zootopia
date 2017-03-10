f7aad458a0bb4d0677bbc3c07c1f6c103fa62944
Aurelia <aurellerua@ymail.com>
2017-03-10 19:29:01 +0700

    Animal

--- src/Animal.h
+++ src/Animal.h
@@ -11,15 +11,17 @@ class Animal {
 		~Animal();
 
 		friend class Zoo;
-		
+
 		virtual void TampilKarakter() = 0;
 		virtual void Sound() = 0;
 		virtual void Move() = 0;
 		//virtual void Interract() = 0;
-	
+		static int GetNbMakanan();
+
 	protected:
 		//tipe makanan
 		//amfibi/tidak
 		//lokasi cell
+		static int NbMakanan();
 };
 #endif
