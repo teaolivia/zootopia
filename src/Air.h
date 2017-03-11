28fd9a175ba26a20755509fd65df8d2bdcac21ce
Aurelia <aurellerua@ymail.com>
2017-03-11 19:18:48 +0700

    add 3 habitat

---
+++ src/Air.h
@@ -0,0 +1,18 @@
+// file Air.h
+
+#ifndef AIR_H
+#define AIR_H
+
+class Air {
+public:
+    Air();
+    ~Air();
+    char GetJenisHabitat();
+
+private:
+    const char JenisHabitat = 'a';
+
+};
+
+#endif // AIR_H
+
