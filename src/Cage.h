b3f6c2f193068718092d23e78b65f611d865fcea
atianadya <theaolivia.mail@gmail.com>
2017-03-12 21:56:32 +0700

    fixing a bit on Cage.h and Cell.h

--- src/Cage.h
+++ src/Cage.h
@@ -1,21 +1,25 @@
 #include <iostream>
+#include "Cell.h"
+#include "Animal.h"
 
 using namespace std;
-
 
 template <class Habitat>
 class Cage : public Cell {
 	public:
 		Cage();
 		~Cage();
-		Cage(habitat const&);
+		Cage(Habitat const&);
 		static int countCell;
 		Habitat getLand();
 		Habitat getWater();
 		Habitat getAir();
+		friend class Cell;
 
-	private:
+	protected:
 		Animal* animal;
     	int length;
     	int width;
+    	// Habitat
+    	Habitat* LandHabitat, WaterHabitat, AirHabitat;
 };
