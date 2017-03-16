d4ebcfb6a02a2748a0b5ca90c0b216be036b538f
aseham <aseham.krazzy@gmail.com>
2017-03-16 09:59:39 +0700

    header

---
+++ src/Main.cpp
@@ -0,0 +1,21 @@
+// Pembuat :
+// Thea Olivia - 13511001
+// Mahesa Gandakusuma - 13513091
+
+#include <iostream>
+#include "animal.h"
+#include "zoo.h"
+#include "matriks.h"
+
+using namespace std;
+
+int main() {
+	cout << "Selamat datang di Zootopia <3" << endl;
+	cout << "Selamat menjelajah <3" << endl;
+	Zoo Z;  //need to generate maps (?)
+	Animal *A;
+	Cage C;
+	Matriks m;
+	Z.Zoo();
+	return 0;
+}
